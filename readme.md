d8d964fbe0169725a3d6b23105976b8bbf4ee3b6
#云淡然 <cloudcome@163.com>
2016-05-18 14:16:20 +0800

    blear.utils.calendar

---
+++ readme.md
@@ -0,0 +1,7 @@
+# blear.utils.calendar
+
+[![Build Status][travis-img]][travis-url] 
+
+[travis-img]: https://travis-ci.org/blearjs/blear.utils.calendar.svg?branch=master
+[travis-url]: https://travis-ci.org/blearjs/blear.utils.calendar
+
---
+++ src/index.js
@@ -0,0 +1,204 @@
+'use strict';
+
+
+var date =   require('blear.utils.date');
+var object = require('blear.utils.object');
+var string = require('blear.utils.string');
+var typeis = require('blear.utils.typeis');
+var fun =    require('blear.utils.function');
+var array =  require('blear.utils.array');
+
+
+/**
+ * 修正 date
+ * @param d
+ * @returns {{year: number, month: number, date: number, day: number}}
+ */
+var fixDateItem = function (d) {
+    return {
+        year: d.getFullYear(),
+        month: d.getMonth(),
+        date: d.getDate(),
+        day: d.getDay()
+    };
+};
+
+
+/**
+ * 是否为今天
+ * @param item
+ * @returns {boolean}
+ */
+var isToday = function (item) {
+    var today = new Date();
+    var todayItem = fixDateItem(today);
+
+    return item.year === todayItem.year &&
+        item.month === todayItem.month &&
+        item.date === todayItem.date;
+};
+
+
+/**
+ * 生成日期ID
+ * @param item {Object} 日对象
+ * @private
+ */
+var generateDateid = function (item) {
+    // 20150218
+    return [item.year, string.padStart(item.month + 1, 2, '0'), string.padStart(item.date, 2, '0')].join('') * 1;
+};
+
+
+/**
+ * 计算开始和结束时间戳
+ * @param item
+ */
+var calItemTime = function (item) {
+    var thisDate = new Date(item.year, item.month, item.date, 0, 0, 0, 0);
+    item.startTime = thisDate.getTime();
+    item.endTime = item.startTime + date.DAY_TIME - 1;
+};
+
+
+/**
+ * 包裹日期
+ * @param [d] {Date|Number} 日期
+ * @returns {Object}
+ */
+exports.wrap = function (d) {
+    d = date.parse(d);
+    var item = fixDateItem(d);
+
+    item.today = isToday(item);
+    item.id = generateDateid(item);
+    calItemTime(item);
+
+    return item;
+};
+
+
+var defaults = {
+    /**
+     * 一周的第一天星期几，默认为周日
+     * @type Number
+     */
+    firstDayInWeek: 0,
+
+    /**
+     * 过滤器
+     * @type Function
+     * @param item
+     * @returns {*}
+     */
+    filter: null
+};
+
+
+/**
+ * 月历
+ * @param year {Number} 年
+ * @param month {Number} 序列月
+ * @param [options] {Object|Function} 配置
+ * @param [options.firstDayInWeek=0] {Number} 一周的第一天星期几，默认为0，即星期日
+ * @param [options.weeks] {undefined|Number} 日历显示几周，默认最小行数，可以指定6+行
+ * @param [options.filter] {null} 过滤器
+ * @returns [Array] 月历数组
+ */
+exports.month = function calendar(year, month, options) {
+    if (typeis.Function(options)) {
+        options = {
+            filter: options
+        };
+    }
+
+    options = object.assign({}, defaults, options);
+
+    var list = [];
+    var prevDate = new Date(year, month - 1);
+    var thisDate = new Date(year, month, 1);
+    var nextDate = new Date(year, month + 1, 1);
+    var thisMonthDays = date.getDaysInMonth(year, month);
+    var thisMonthFirstDateDay = thisDate.getDay();
+    var firstDayInWeek = options.firstDayInWeek;
+    thisMonthFirstDateDay = thisMonthFirstDateDay < firstDayInWeek ? thisMonthFirstDateDay + 7 : thisMonthFirstDateDay;
+    var deltaDays = thisMonthFirstDateDay - firstDayInWeek;
+    var prevMonthDays = date.getDaysInMonth(year, month - 1);
+    var i = 0;
+
+    // 上月
+    for (; i < deltaDays; i++) {
+        list.push({
+            year: prevDate.getFullYear(),
+            month: prevDate.getMonth(),
+            date: prevMonthDays - i,
+            prevMonth: true
+        });
+    }
+
+    list.reverse();
+
+    // 本月
+    for (i = 1; i <= thisMonthDays; i++) {
+        list.push({
+            year: thisDate.getFullYear(),
+            month: thisDate.getMonth(),
+            date: i,
+            thisMonth: true
+        });
+    }
+
+    deltaDays = list.length % 7;
+
+    // 下月
+    if (deltaDays) {
+        for (i = 1; i <= 7 - deltaDays; i++) {
+            list.push({
+                year: nextDate.getFullYear(),
+                month: nextDate.getMonth(),
+                date: i,
+                nextMonth: true
+            });
+        }
+    }
+
+    var weeks = Math.ceil(list.length / 7);
+    var optionWeeks = options.weeks;
+    if (typeis.Number(optionWeeks) && optionWeeks > weeks) {
+        // 下个月
+        var j = (optionWeeks - weeks) * 7;
+
+        while (j--) {
+            list.push({
+                year: nextDate.getFullYear(),
+                month: nextDate.getMonth(),
+                date: i++,
+                nextMonth: true
+            });
+        }
+    }
+
+    // 分组
+    var group = [];
+    var index = 0;
+    var filter = fun.noop(options.filter);
+
+    while (list.length) {
+        var groupItem = list.splice(0, 7);
+        array.each(groupItem, function (dayIndex, item) {
+            item.id = generateDateid(item);
+            item.prevMonth = item.prevMonth || false;
+            item.thisMonth = item.thisMonth || false;
+            item.nextMonth = item.nextMonth || false;
+            item.today = isToday(item);
+            calItemTime(item);
+            item.day = (dayIndex + firstDayInWeek) % 7;
+            item.week = index;
+            filter(item, index);
+        });
+        group.push(groupItem);
+        index++;
+    }
+
+    return group;
+};
